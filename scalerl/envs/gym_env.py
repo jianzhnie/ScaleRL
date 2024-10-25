a2461dae50ce60f9a764229c9f980f964543fc19
jianzhnie <jianzhnie@126.com>
2024-10-25 17:51:50 +0800

    Update scalerl envs

--- scalerl/envs/gym_env.py
+++ scalerl/envs/gym_env.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+import numpy as np
 from gymnasium.wrappers import RecordEpisodeStatistics
 
 
@@ -30,3 +31,36 @@ def make_gym_env(
     env = gym.wrappers.RecordEpisodeStatistics(env)
     env.action_space.seed(seed)
     return env
+
+
+class GymEnvWrapper(gym.Env):
+    """A wrapper class for the Gymnasium environment.
+
+    Attributes:
+        obs_dim (int): The dimension of the obs space.
+        action_dim (int): The dimension of the action space.
+        is_discrete (bool): Whether the action space is discrete.
+        max_episode_steps (int): The maximum number of steps per episode.
+        can_run (bool): Whether the environment is ready to run.
+        obs (Optional[Union[int, float, Tuple[float]]]): The current obs of the environment.
+    """
+
+    def __init__(self, env: gym.Env) -> None:
+        """Initializes the Environment with the given Gymnasium environment.
+
+        Args:
+            env_name (str): The name of the Gymnasium environment.
+        """
+        self.env = env
+        self.obs_shape = env.observation_space.shape or env.observation_space.n
+        self.action_shape = env.action_space.shape or env.action_space.n
+        self.obs_dim = int(np.prod(self.obs_shape))
+
+        # Determine action space type and dimension
+        if isinstance(self.env.action_space,
+                      gym.spaces.box.Box):  # Continuous action space
+            self.action_dim = self.env.action_space.shape[0]
+            self.is_discrete = False
+        else:  # Discrete action space
+            self.action_dim = self.env.action_space.n
+            self.is_discrete = True
