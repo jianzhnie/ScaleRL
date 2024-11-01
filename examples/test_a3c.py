0ab399856d5ffcb2eb132de5546480b041c51238
jianzhnie <jianzhnie@126.com>
2024-11-01 16:19:11 +0800

    Update scalerl

--- examples/test_a3c.py
+++ examples/test_a3c.py
@@ -6,5 +6,5 @@ from scalerl.algorithms.a3c.parallel_a3c import ParallelA3C
 
 if __name__ == '__main__':
     os.environ['OMP_NUM_THREADS'] = '1'
-    a3c = ParallelA3C(env_name="CartPole-v0")
+    a3c = ParallelA3C(env_name='CartPole-v0')
     a3c.run()
